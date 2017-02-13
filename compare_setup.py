b18e1e677aad3dd329cd8527a5b1fbd9c59444fc
Dijkstra7 <rickr.dijkd@gmail.com>
2017-02-13 14:32:42 +0100

    Different approach to coding Spatial coding model
    The paper has not mentioned several equations, such as how to get the
    competing clone.
    I've added the Reciever class, which has the clones in it.

--- compare_setup.py
+++ compare_setup.py
@@ -7,9 +7,10 @@ Created on Tue Feb 07 11:31:39 2017
 # Trying to make this according to commenting standards
 
 # The imports.
-from random import random
+import random
 
 from combined_model import CombinedModel
+from spatial_model import SpatialModel
 
 
 def words_to_be_compared():
@@ -18,7 +19,7 @@ def words_to_be_compared():
     TODO:
     - kiezen om een lijst hier te hardcoden of om het uit een file te halen.
     """
-    some_words = ''
+    some_words = ['stop', 'stoop']
     return some_words
 
 
@@ -31,24 +32,24 @@ def get_similarity_words(base_word, compare_word, method_='Open Bigrams'):
     - choose what models to use and implement as class
     - implement all the classes basic operations
     """
-    similarity_score = 0
+    sim_score = 0
     if method_ == 'Open Bigrams':
-        similarity_score = BigramModel(base_word, compare_word).sim_score
+        sim_score = BigramModel(base_word, compare_word).similarity_score
     if method_ == 'Spatial Coding':
-        similarity_score = SpatialModel(base_word, compare_word).sim_score
+        sim_score = SpatialModel(base_word, compare_word).similarity_score
     if method_ == 'Combined Method':
-        similarity_score = CombinedModel(base_word, compare_word).sim_score
+        sim_score = CombinedModel(base_word, compare_word).similarity_score
     if method_ == 'Holo Open Bigrams':
-        similarity_score = HoloModel(base_word, compare_word).sim_score
-    return similarity_score
+        sim_score = HoloModel(base_word, compare_word).similarity_score
+    return sim_score
 
 
 def compare_words(method=None):
     # Take two random words from the list of words and get a similarityscore.
     words = words_to_be_compared()
-    base_word = words[random.randint(0, len(words))]
-    compare_word = words[random.randint(0, len(words))]
+    base_word = words[random.randint(0, len(words)-1)]
+    compare_word = words[random.randint(0, len(words)-1)]
     return get_similarity_words(base_word, compare_word, method)
 
 
-print compare_words()
+print compare_words('Spatial Coding')
--- spatial_model.py
+++ spatial_model.py
@@ -6,6 +6,21 @@ Created on Fri Feb 10 14:29:49 2017
 """
 
 # Imports
+import math
+
+
+def closest_to_0(current, contender):
+    if abs(current)-abs(contender) > 0:
+        return contender
+    return current
+
+
+def closer_to_0(current, contender):
+    if contender is None:
+        return False
+    if abs(current)-abs(contender) > 0:
+        return True
+    return False
 
 
 class SpatialModel:
@@ -22,11 +37,34 @@ class SpatialModel:
     sigma_0 = 0.48  # As stated in table 3 in spatial coding.
     k_0 = 0.24      # As stated in table 3 in spatial coding.
     sigma = 0
+    bank_of_receivers = []   # Not sure if needed
+    base_word = 'spam'
+    compare_word = 'eggs'
 
     def __init__(self, base_word, compare_word):
-        self.sigma = calculate_sigma(len(compare_word))
-        self.similarity_score = \
-            self.calculate_similarity(base_word, compare_word)
+        self.sigma = self.calculate_sigma(len(compare_word))
+        # self.similarity_score = \
+        #    self.calculate_similarity(base_word, compare_word)
+        self.initialise_receivers(base_word, compare_word)
+        self.base_word = base_word
+        self.compare_word = compare_word
+
+    def initialise_receivers(self, template, input_):
+        """
+        initialise the receivers from the template, and activate them by
+        finding the winning receivers
+        """
+        for position, identity in enumerate(template):
+            self.bank_of_receivers.append(Receiver(identity, len(input_),
+                                                   position))
+        for position, identity in enumerate(input_):
+            for bank_pos, bank in enumerate(self.bank_of_receivers):
+                bank.update_clones(identity, position, bank_pos)
+        for bank in self.bank_of_receivers:
+            if bank.winning_clone is not None:
+                bank.cross_bank_winner(self.bank_of_receivers)
+        for bank in self.bank_of_receivers:
+            bank.printself()
 
     def spatial(self, letter_pos, goal_pos):
         """ equation 1 in spatial coding
@@ -44,7 +82,7 @@ class SpatialModel:
         the assumption that longer words wil have bigger sigma's is
         implemented here.
         """
-        self.sigma = sigma_0+k_0*length
+        self.sigma = self.sigma_0+self.k_0*length
 
     def activation(self, letter, time):
         """ calculate the activation level of a letter at a time.
@@ -59,8 +97,8 @@ class SpatialModel:
 
         TODO: figure out if activation should be impplemented differently
         """
-        activation_letter = activation(letter, time)
-        return activation_letter*spatial(letter_pos, goal_pos)
+        activation_letter = self.activation(letter, time)
+        return activation_letter*self.spatial(letter_pos, goal_pos)
 
     def delay(goal_pos, letter_pos):
         """ the delay implemented by the SC model
@@ -70,32 +108,32 @@ class SpatialModel:
         """
         return goal_pos-letter_pos
 
-    def reciever(self, bank, channel, identity, letter, position, time):
+    def receiver(self, bank, channel, identity, letter, position, time):
         """ equation 9 in spatial coding.
 
-        This equation calculates the activation of a reciever in a bank on a
+        This equation calculates the activation of a receiver in a bank on a
         channel.
         The bank is the expected letter position.
 
         Not sure if identity or channel has to be implemented. in the SC paper
         it stands for the identity of the i'th word.
         """
-        return signal(bank, position, time, letter)-delay(bank, position)
+        return self.signal(bank, position, time, letter)-self.delay(bank,
+                                                                    position)
 
-    def winning_reciever(recievers, position, time):
-        """ calculate the score of the winning reciever
+    def winning_receiver(self, receivers, position, time):
+        """ calculate the score of the winning receiver
 
-        The winning reciever is that reciever that is closest to 0.
+        The winning receiver is that receiver that is closest to 0.
         """
         winning_score = 99
-        for (rec_letter, rec_pos) in recievers:
-            rec_score = reciever(rec_pos, None, None, rec_letter, position,
-                                 time)
-            if abs(abs(winning_score) - abs(rec_score)) > 0:
-                winning_score = rec_score
+        for (rec_letter, rec_pos) in receivers:
+            rec_score = self.receiver(rec_pos, None, None, rec_letter,
+                                      position, time)
+            winning_score = closest_to_0(winning_score, rec_score)
         return winning_score
 
-    def super_position(banks_of_recievers, identity, position, time):
+    def super_position(self, banks_of_receivers, identity, position, time):
         """ equation 10 in spatial coding.
 
         'The superposition function is found by summing across the receiver
@@ -103,11 +141,76 @@ class SpatialModel:
 
         """
         super_position_score = 0
-        for bank in banks_of_recievers:
-            winning_reciever_score = winning_reciever(bank, position, time)
+        for bank in banks_of_receivers:
+            winning_receiver_score = self.winning_receiver(bank, position,
+                                                           time)
             super_position_score = super_position_score + \
-                winning_reciever_score
+                winning_receiver_score
         return super_position_score
 
-    def calculate_similarity(self, baseword, compare_word):
-        reutn 'to be mplemented'
+    def match(self, banks_of_receivers, base_length, compare_length, time):
+        """ equation 7 in spatial coding
+
+        matching the base_word with the compare_word.
+        """
+        match = 0
+        res_phase = self.find_resonating_phase(abs(base_length - 
+                                                   compare_length))
+
+    def find_resonating_phase(max_dist):
+        best_pos = (0,0)       
+        for pos in range(-1*max_dist, max_dist):
+            'Do things'
+
+    def calculate_similarity(self, time):
+        return 1  # receiver_output(time)+ext_letter_match(time)
+
+    def ext_letter_match(time):
+        for i in pie:
+            'eat sky'
+
+
+class Receiver:
+    """
+    the structure of a receiver
+    """
+    identity = 'a'
+    clones = []
+    position = -1
+    winning_clone = None
+
+    def __init__(self, identity, n_clones, position):
+        self.identity = identity
+        clones = []
+        for clone in range(n_clones):
+            clones.append(None)
+        self.clones = clones
+        self.position = position
+
+    def update_clones(self, identity_input, pos_input, pos_self):
+        if self.identity == identity_input:
+            self.clones[pos_input] = pos_input-pos_self
+        self.select_winner()
+
+    def select_winner(self):
+        winning_clone = 99
+        for id_clone, clone in enumerate(self.clones):
+            if closer_to_0(winning_clone, clone):
+                self.winning_clone = id_clone
+                winning_clone = clone
+
+    def cross_bank_winner(self, bank):
+        for other_clones in bank:
+            if self.identity == other_clones.identity:
+                if closer_to_0(self.clones[self.winning_clone],
+                               other_clones.clones[self.winning_clone]):
+                    self.winning_clone = None
+                    break
+
+    def printself(self):
+        print self.identity
+        for id_, clone in enumerate(self.clones):
+            suffix = ''
+            if id_ == self.winning_clone:
+                suffix = ' <= winner'
+            print str(id_+1)+" "+str(clone)+suffix
